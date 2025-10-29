97e6cfcffe7034c5c2e4911ded06583502aba982
Denae Hawkins <hawkinsdenae@gmail.com>
2025-10-28 23:32:19 -0700

    targeted the form section with JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// This is for f12 in console you can check if its all connected
 console.log("JavaScript is connected!");
+
+// This section will be selecting all the parts that needs javascript this specifically targets the form section in HTML, step 1 after connecting to html
+const form = document.getElementById('password-form');
+const passwordInput = document.getElementById('password-input');
+const resultsDiv = document.getElementById('results');
+
