5c60956f56248b7a525741ddb74eae7be354785f
Denae Hawkins <hawkinsdenae@gmail.com>
2025-10-29 22:18:34 -0700

    added the ability to collect data from the form submitted

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-// This is for f12 in console you can check if its all connected
-console.log("JavaScript is connected!");
+console.log("JavaScript is connected!");  // Connects to HTML
 
 // This section will be selecting all the parts that needs javascript this specifically targets the form section in HTML, step 1 after connecting to html
 const form = document.getElementById('password-form');
 const passwordInput = document.getElementById('password-input');
 const resultsDiv = document.getElementById('results');
 
+// This is for f12 in console, in Javascript you have to connect each element you'll be working with
+console.log(form);
+console.log(passwordInput);
+console.log(resultsDiv);
+
+// This section collects the input value that is going into the password checker
+form.addEventListener('submit', function(buttonEvent) {
+    buttonEvent.preventDefault();
+    console.log("Form submitted!");  // This is to check if the form is working
+    let userPassword = passwordInput.value;
+    console.log(userPassword);
+});
